bb3423380c578ec763dd412d0aea71feebed8a86
HyungJin <hyungjin0710@gmail.com>
2021-12-14 02:25:35 +0900

    #5.5 To Do Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
+import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
+
+const GlobalStyle = createGlobalStyle``;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "white",
+  accentColor: "#00a8ff",
+  cardBgColor: "transparent",
+};
