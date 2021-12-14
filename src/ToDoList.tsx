3783a14fb34df15f6c1257499deae83cf8814f8c
HyungJin <hyungjin0710@gmail.com>
2021-12-14 17:46:07 +0900

    #5.8 Form Errors

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -22,35 +22,66 @@ import { useForm } from "react-hook-form";
 //   );
 // }
 
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+}
+
 function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
   const onValid = (data: any) => {
     console.log(data);
   };
-  console.log(formState.errors);
+
   return (
     <div>
       <form
         style={{ display: "flex", flexDirection: "column" }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emamils allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
         <input
           {...register("firstName", { required: true })}
           placeholder="First Name"
         />
+        <span>{errors?.firstName?.message}</span>
+
         <input
           {...register("lastName", { required: true })}
           placeholder="Last Name"
         />
+        <span>{errors?.lastName?.message}</span>
+
         <input
           {...register("username", { required: true, minLength: 10 })}
           placeholder="Username"
         />
+        <span>{errors?.username?.message}</span>
+
         <input
           {...register("password", { required: true, minLength: 5 })}
           placeholder="Password"
         />
+        <span>{errors?.password?.message}</span>
+
         <input
           {...register("password1", {
             required: "Password is required",
@@ -61,6 +92,8 @@ function ToDoList() {
           })}
           placeholder="Password1"
         />
+        <span>{errors?.password1?.message}</span>
+
         <button>Add</button>
       </form>
     </div>
